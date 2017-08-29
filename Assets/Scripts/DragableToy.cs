f9eb4ade402bf7e1cf9eeb6438fdabf85ca1c801
DennisGL1 <dennis@gamelevelone.com>
2017-08-29 18:31:54 +0700

    added new tag, added toy sprite, animation, script

---
+++ Assets/Scripts/DragableToy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DragableToy : MonoBehaviour {
+
+	public void BeginDrag()
+	{
+		GetComponent<Animator>().SetBool("hold",true);
+		GetComponent<CircleCollider2D>().enabled = false;
+		GetComponent<Rigidbody2D>().simulated = false;
+	}
+
+	public void Drag()
+	{
+		Vector3 tempMousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,10f);
+		transform.localPosition = Camera.main.ScreenToWorldPoint(tempMousePosition);
+	}
+
+	public void EndDrag()
+	{
+		GetComponent<CircleCollider2D>().enabled = true;
+		GetComponent<Rigidbody2D>().simulated = true;
+		GetComponent<Animator>().SetBool("hold",false);
+	}
+}
