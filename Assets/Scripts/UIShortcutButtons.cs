8066a5a94a7581f97eada69bf9553835c5607d07
Helga <helga@gamelevelone.com>
2017-08-29 19:03:04 +0700

    UI shortcut animation

---
+++ Assets/Scripts/UIShortcutButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIShortcutButtons : MonoBehaviour {
+	public GameObject PanelUIShortcut;
+	public Animator buttonAnim;
+	string triggerOpen = "Open";
+	string triggerClose = "Close";
+
+	public void OpenMenu(){
+		PanelUIShortcut.SetActive(true);
+		buttonAnim.SetTrigger(triggerOpen);
+	}
+
+	public void CloseMenu(){
+		buttonAnim.SetTrigger(triggerClose);
+		StartCoroutine(WaitForAnim());
+	}
+
+	IEnumerator WaitForAnim(){
+		yield return new WaitForSeconds(1);
+		PanelUIShortcut.SetActive(false);
+	}
+}
