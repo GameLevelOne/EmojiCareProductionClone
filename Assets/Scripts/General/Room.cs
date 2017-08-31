e0501c9e5b0bc205689cfe8781683100226ca2e8
DennisGL1 <dennis@gamelevelone.com>
2017-08-31 17:45:14 +0700

    added general scripts (strings, room, furniture(movable and immovable), emoji, player, etc.

---
+++ Assets/Scripts/General/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Room : MonoBehaviour {
+	#region attribute
+
+	#endregion
+
+
+	#region mechanics
+
+	#endregion
+
+
+	#region public methods
+
+	#endregion
+}
