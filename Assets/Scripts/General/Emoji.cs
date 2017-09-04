77a63be9304a7c19bc5a71bb64d85fc38da0f1d6
Helga <helga@gamelevelone.com>
2017-09-04 18:39:06 +0700

    scene selection - generate-choose emoji

--- Assets/Scripts/General/Emoji.cs
+++ Assets/Scripts/General/Emoji.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Emoji : MonoBehaviour {
+	private Emoji instance;
+	private EmojiSO currentEmojiData;
 
-	
+	public Emoji Instance {
+		get{ return instance;}
+	}
 
+	void Awake(){
+		if(instance != null && instance != this){
+			Destroy(this.gameObject);
+		} else{
+			instance = this;
+		}
+	}
 }
---
+++ Assets/Scripts/SceneSelection/EmojiSelectionData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EmojiSelectionData : MonoBehaviour, IPointerClickHandler {
+	public EmojiSO currEmojiData;
+	public GameObject priceBox;
+	public PopupSelection confirmationPopup;
+	public Text textPrice;
+
+	public delegate void EmojiClicked();
+	public static event EmojiClicked OnEmojiClicked;
+
+	public void InitEmoji(EmojiSO emojiData){
+		currEmojiData = emojiData;
+		if(emojiData.isUnlocked){
+			priceBox.SetActive(false);	
+		} else{
+			priceBox.SetActive(true);
+			textPrice.text = emojiData.emojiPrice.ToString();
+		}
+		gameObject.name = emojiData.emojiName;
+		//gameObject.GetComponent<Image>().sprite = emojiSO[idx].emojiSelectionIcon;
+	}
+
+	public void OnPointerClick(PointerEventData ped){
+		PopupSelection.Instance.ShowPopup(currEmojiData);
+	}
+
+}
