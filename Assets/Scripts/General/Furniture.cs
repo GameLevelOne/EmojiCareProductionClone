2bc9ce6c0482448222507829f7b4a5f199d0dfe8
DennisGL1 <dennis@gamelevelone.com>
2017-09-05 18:15:40 +0700

    modified some parent scripts, added main camera prefab, optimized parent classes, optimize room swiping. added new sceneMain for main game

--- Assets/Scripts/General/Furniture.cs
+++ Assets/Scripts/General/Furniture.cs
@@ -4,4 +4,13 @@ using UnityEngine.UI;
 
 public class Furniture : MonoBehaviour {
 	public bool editMode = false;
+
+	public void SwitchEditMode(bool editMode)
+	{
+		if(editMode){
+			//enter edit mode
+		}else{
+			//exit edit mode
+		}
+	}
 }
