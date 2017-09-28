849e685572d3c179b255e405548dce592b2d9037
Helga <helga@gamelevelone.com>
2017-09-28 18:29:11 +0700

    implement panel progress and album assets

--- Assets/Scripts/UI/PanelAlbumManager.cs
+++ Assets/Scripts/UI/PanelAlbumManager.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelAlbumManager : MonoBehaviour {
 	public GameObject emojiBoxPrefab;
 	public RectTransform emojiContentBox;
+	public Scrollbar scrollbar;
 
 	int totalEmoji = 10;
 	int tileWidth = 3;
 	int tileHeight = 4;
-	float boxSize = 200f;
-	float contentBoxMarginX = 100;
+	float boxSize = 150;
+	float contentBoxMarginX = 50;
 
 	void Start(){
 		InitContentBox ();
@@ -23,8 +24,9 @@ public class PanelAlbumManager : MonoBehaviour {
 		for (int i = 0; i < tileHeight; i++) {
 			for (int j = 0; j < tileWidth; j++) {
 				GameObject obj = Instantiate(emojiBoxPrefab,emojiContentBox,false) as GameObject;
-				obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-200+j*200,340-i*200);
+				obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-140+j*140,230-i*150);
 			}
 		}
+		scrollbar.size = 0.1f;
 	}
 }
