aa7bf79e64d0a2e2871aededc137b89a7aa38b39
Helga <helga@gamelevelone.com>
2017-09-29 16:47:58 +0700

    UI progress and album with temp data

--- Assets/Scripts/UI/PanelAlbumManager.cs
+++ Assets/Scripts/UI/PanelAlbumManager.cs
@@ -8,6 +8,9 @@ public class PanelAlbumManager : MonoBehaviour {
 	public RectTransform emojiContentBox;
 	public Scrollbar scrollbar;
 
+	public Sprite[] emojiIcons = new Sprite[12];
+	public Sprite lockedEmoji;
+
 	int totalEmoji = 10;
 	int tileWidth = 3;
 	int tileHeight = 4;
@@ -18,13 +21,22 @@ public class PanelAlbumManager : MonoBehaviour {
 		InitContentBox ();
 	}
 
-	void InitContentBox(){
+	void InitContentBox ()
+	{
+		int tempIdx = 0;
 		emojiContentBox.sizeDelta = new Vector2 (0, ((float)tileHeight * boxSize) + contentBoxMarginX);
 
 		for (int i = 0; i < tileHeight; i++) {
 			for (int j = 0; j < tileWidth; j++) {
-				GameObject obj = Instantiate(emojiBoxPrefab,emojiContentBox,false) as GameObject;
-				obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-140+j*140,230-i*150);
+				GameObject obj = Instantiate (emojiBoxPrefab, emojiContentBox, false) as GameObject;
+				obj.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (-140 + j * 140, 230 - i * 150);
+				if (emojiIcons [tempIdx] != null) {
+					Debug.Log("asd");
+					obj.transform.GetChild (0).GetComponent<Image> ().sprite = emojiIcons [tempIdx];
+				}else{
+					obj.transform.GetChild(0).GetComponent<Image>().sprite = lockedEmoji;
+				}
+				tempIdx++;
 			}
 		}
 		scrollbar.size = 0.1f;
--- Assets/Scripts/UI/PanelProgressManager.cs
+++ Assets/Scripts/UI/PanelProgressManager.cs
@@ -14,6 +14,9 @@ public class PanelProgressManager : MonoBehaviour {
 	public GameObject expressionBoxPrefab;
 	public RectTransform contentBox;
 
+	public Sprite[] expressionIcons = new Sprite[40];
+	public Sprite lockedExpression;
+
 	int expressionTotalCount = 40;
 	int tileWidth = 4;
 	int tileHeight = 10;
@@ -25,12 +28,36 @@ public class PanelProgressManager : MonoBehaviour {
 	}
 
 	void InitContentBox(){
+		int exprTileIdx = 0;
+		int unlockedExprIdx = 0;
+		List<FaceAnimation> exprList = new List<FaceAnimation>(); //temp
+
+		//for testing
+		exprList.Add(FaceAnimation.Default);
+		exprList.Add(FaceAnimation.Amused);
+		exprList.Add(FaceAnimation.Cry);
+		exprList.Add(FaceAnimation.Fidget);
+
 		contentBox.sizeDelta = new Vector2(0,((float)tileHeight*expressionBoxWidth)+contentBoxMarginX);
 
 		for(int i=0;i<tileHeight;i++){
 			for(int j=0;j<tileWidth;j++){
 				GameObject obj = Instantiate(expressionBoxPrefab,contentBox,false) as GameObject;
 				obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-160+j*105,500-i*105);
+
+				if(unlockedExprIdx < exprList.Count){
+					if((int)exprList[unlockedExprIdx] == (exprTileIdx-1)){
+						obj.transform.GetChild(0).GetComponent<Image>().sprite = expressionIcons[exprTileIdx-1];
+						unlockedExprIdx++;
+					}else{
+						obj.transform.GetChild(0).GetComponent<Image>().sprite = lockedExpression;
+					}
+
+				}else{
+					obj.transform.GetChild(0).GetComponent<Image>().sprite = lockedExpression;
+				}
+
+				exprTileIdx++;
 			}
 		}
 	}
