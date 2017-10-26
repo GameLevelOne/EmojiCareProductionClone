f5f74d0d8953ea29192ffa62c24cd070dc86fcdd
Helga <helga@gamelevelone.com>
2017-10-26 09:59:17 +0700

    UI revisions

--- Assets/Scripts/UI/Minigames/BricksMinigame.cs
+++ Assets/Scripts/UI/Minigames/BricksMinigame.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class BricksMinigame : BaseUI {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Transform[] objects;
+	Vector3[] objStartPos = new Vector3[6];
+
+	void Start(){
+//		for(int i=0;i<objects.Length;i++){
+//			objStartPos[i] = objects[i].localPosition;
+//		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public override void InitUI ()
+	{
+//		for(int i=0;i<objects.Length;i++){
+//			objects[i].localPosition = objStartPos[i];
+//		}
+		//StartCoroutine(WaitForAnim());
+	}
+
+	IEnumerator WaitForAnim(){
+		SimulatePhysics(false);
+		yield return new WaitForSeconds(0.16f);
+		SimulatePhysics(true);
+	}
+
+	void SimulatePhysics(bool doSimulate){
+		for(int i=0;i<objects.Length;i++){
+			if(doSimulate)
+				objects[i].GetComponent<Rigidbody2D>().simulated=true;
+			else
+				objects[i].GetComponent<Rigidbody2D>().simulated=false;
+		}
 	}
 }
---
+++ Assets/Scripts/UI/ScreenShopContent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShopContent : BaseUI {
+	public ScreenShops screenShop;
+	public GameObject parentShopChoice;
+
+	public void CloseShopContent(){
+		screenShop.UpdateButtonDisplay();
+		base.ShowUI(parentShopChoice);
+		base.CloseUI(this.gameObject);
+	} 
+	
+}
