4773a5f63ee8685ad9d6923a330039c2f0a25336
DennisGL1 <dennis@gamelevelone.com>
2017-11-07 18:32:16 +0700

    added ui progress for gardening, optimized watering can, modified scene main

--- Assets/Scripts/SceneMain/Rooms/Garden/WateringCan.cs
+++ Assets/Scripts/SceneMain/Rooms/Garden/WateringCan.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class WateringCan : MonoBehaviour {
 	#region attributes
 	public Collider2D wateringCanTrigger;
+	public SpriteRenderer thisSprite;
+	public Vector2 offset = new Vector2(0.5f,0.3f);
 	Vector3 startPos;
 	#endregion
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -19,17 +21,20 @@ public class WateringCan : MonoBehaviour {
 	{
 		wateringCanTrigger.enabled = true;
 		wateringCanTrigger.gameObject.GetComponent<WateringCanTrigger>().Water();
+		thisSprite.sortingLayerName = SortingLayers.HELD;
 	}
 
 	public void Drag()
 	{
 		Vector3 tempMousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,19f);
-		transform.position = Camera.main.ScreenToWorldPoint(tempMousePosition);
+		Vector3 newPos = Camera.main.ScreenToWorldPoint(tempMousePosition);
+		transform.position = new Vector3(newPos.x+offset.x,newPos.y+offset.y,-1);
 	}
 
 	public void EndDrag()
 	{
 		wateringCanTrigger.gameObject.GetComponent<WateringCanTrigger>().Stop();
+		thisSprite.sortingLayerName = SortingLayers.MOVABLE_FURNITURE;
 		StartCoroutine(Return());
 	}
 	#endregion
---
+++ Assets/Scripts/UI/UIPlantProgress.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UIPlantProgress : MonoBehaviour {
+	#region attributes
+	public Image imagePlantIcon;
+	public Image imageProgressBar;
+	public Text textTimeLeft;
+	int totalTime;
+	DateTime finishTime;
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region initialization
+	public void Init(Image icon, DateTime finishTime)
+	{
+		this.imagePlantIcon = icon;
+		this.finishTime = finishTime;
+	}
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region mechanics
+	int getDuration()
+	{
+		TimeSpan duration = finishTime.Subtract(DateTime.Now);
+		int hour = duration.Hours;
+		int minute = duration.Minutes;
+		int second = duration.Seconds;
+
+		int totalSec = hour * 60 * 60 + minute * 60 + second;
+		return totalSec;
+	}
+
+	string ConvertTimeToString()
+	{
+		TimeSpan duration = finishTime.Subtract(DateTime.Now);
+
+		return 	duration.Hours.ToString()+"h "+
+				duration.Minutes.ToString()+"m "+
+				duration.Seconds.ToString()+"s";
+	}
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region public modules
+	public void UpdateData()
+	{
+		if(!gameObject.activeSelf) gameObject.SetActive(true);
+		textTimeLeft.text = ConvertTimeToString();
+	}
+
+	public void Hide()
+	{
+		gameObject.SetActive(false);
+	}
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------	
+	#region coroutine
+	const string _TickTime = "TickTime";
+	IEnumerator TickTime()
+	{
+		while(DateTime.Now.CompareTo(finishTime) < 0){
+			textTimeLeft.text = ConvertTimeToString();
+			yield return new WaitForSeconds(1f);
+		}
+
+		Hide();
+	}
+	#endregion
+}
