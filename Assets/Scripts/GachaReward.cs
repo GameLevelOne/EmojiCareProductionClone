76d55838514ea1d905f2ce5610d96649ec7bdde7
Helga <helga@gamelevelone.com>
2017-11-10 16:48:02 +0700

    update block minigame,gacha reward scratch

--- Assets/Scripts/GachaReward.cs
+++ Assets/Scripts/GachaReward.cs
@@ -13,9 +13,15 @@ public class GachaReward : MonoBehaviour {
 	float rateIngredients = 0f;
 	float rateCostume = 0f;
 
+	int minGem = 0;
+	int maxGem = 0;
+	int minCoin = 0;
+	int maxCoin = 0;
+
 	void Start(){
 		//TEMP
 		SetGachaRates (0.7f, 0.05f, 0.2f, 0.05f);
+		SetMinMaxCoinGem (10, 100, 1, 3);
 	}
 
 	public void SetGachaRates(float rateCoin,float rateGem,float rateIngredients,float rateCostume){
@@ -23,6 +29,13 @@ public class GachaReward : MonoBehaviour {
 		this.rateGem = rateGem;
 		this.rateIngredients = rateIngredients;
 		this.rateCostume = rateCostume;
+	}
+
+	public void SetMinMaxCoinGem(int minCoin,int maxCoin,int minGem,int maxGem){
+		this.minCoin = minCoin;
+		this.maxCoin = maxCoin;
+		this.minGem = minGem;
+		this.maxGem = maxGem;
 	}
 
 	public void StartGacha (){
@@ -41,13 +54,13 @@ public class GachaReward : MonoBehaviour {
 
 	void ProcessReward(RewardType type){
 		if(type == RewardType.Gem){
-			
+			PlayerData.Instance.PlayerGem = Random.Range (minGem, (maxGem + 1));
 		} else if(type == RewardType.Costume){
 			
 		} else if(type == RewardType.Ingredients){
 			
 		} else if(type == RewardType.Coin){
-			
+			PlayerData.Instance.PlayerCoin = Random.Range (minCoin, (maxCoin + 1));
 		}	
 	}
 
