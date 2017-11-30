f4ffb08cc25c377b74b9f03ed80fcbcc3c42a59a
DennisGL1 <dennis@gamelevelone.com>
2017-11-30 11:26:36 +0700

    - modified scene main * added hatstand object
    - added hatstand script
    - added hatstand SOScript and SOData
    
    - modified emoji
    * added new bubble assets and animation

---
+++ Assets/Scripts/SOClass/HatSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Hat_",menuName = "SOData/Hat",order = 3)]
+public class HatSO : ScriptableObject {
+	public string ID = "Hat_";
+	public int price = 0;
+}
