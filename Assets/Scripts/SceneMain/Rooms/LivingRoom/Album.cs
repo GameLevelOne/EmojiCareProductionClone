9ee1dbf83136751719c6f96a61336c6898016095
DennisGL1 <dennis@gamelevelone.com>
2017-11-30 12:37:41 +0700

    - modified scene main *added envelope, album, magnifying glass object.

---
+++ Assets/Scripts/SceneMain/Rooms/LivingRoom/Album.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Album : ActionableFurniture {
+	#region attributes
+	[Header("Album Attributes")]
+	public ScreenAlbum UIAlbum;
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region initialization
+	public override void InitVariant ()
+	{
+		base.InitVariant ();
+
+
+		if(PlayerData.Instance.EmojiAlbumData.Count <= 0) gameObject.SetActive(false);
+	}
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region mechanics
+	public override void PointerClick ()
+	{
+		//open album ui
+		UIAlbum.ShowUI(UIAlbum.gameObject);
+	} 
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region public modules
+	
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------	
+}
---
+++ Assets/Scripts/SceneMain/Rooms/LivingRoom/Envelope.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Envelope : ActionableFurniture {
+	#region attributes
+	[Header("Envelope Attributes")]
+	public ScreenProgress UIProgress;
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region initialization
+	
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region mechanics
+	public override void PointerClick ()
+	{
+		UIProgress.ShowUI(UIProgress.gameObject);
+	}
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+	#region public modules
+	
+	#endregion
+//-------------------------------------------------------------------------------------------------------------------------------------------------	
+}
