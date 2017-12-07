4aab346193e91c8ae7f6646dec9d0378faa598bb
DennisGL1 <dennis@gamelevelone.com>
2017-12-07 12:47:46 +0700

    ADJUSTMENT
    - new gacha animation, asset, mechanics
    *add gacha counter in gacha screen
    *player has option to go back (not opening gacha), or open gacha
    *add shining effect to gacha reward
    *add text to gacha reward if the reward is ingredient
    
    - modified scene main

---
+++ Assets/Scripts/UI/GachaAnimationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GachaAnimationEvent : MonoBehaviour {
+	public GachaReward gachaParent;
+
+	public void EnableTouchButton()
+	{
+		gachaParent.touchArea.interactable = true;
+		gachaParent.buttonBack.SetActive(true);
+	}
+	public void DisableTouchButton()
+	{
+		gachaParent.touchArea.interactable = false;
+		gachaParent.buttonBack.SetActive(false);
+	}
+	public void ShowShining()
+	{
+		gachaParent.shiningImage.SetActive(true);
+	}
+	public void HideShining()
+	{
+		gachaParent.shiningImage.SetActive(false);
+	}
+}
